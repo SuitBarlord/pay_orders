6d87198997c7cef42c34f120432c68fc9e030fb5
SuitBarlord <dolgovNA.99@yandex.ru>
2024-02-26 20:49:10 +0900

    перед filter

--- payorders/settings.py
+++ payorders/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'engine',
     'crispy_forms',
     'crispy_bootstrap4',
+    'django_filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
