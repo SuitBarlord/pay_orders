b9d7bdd3d34c21317307c4781b7cf34e6d4d0fb4
SuitBarlord <dolgovNA.99@yandex.ru>
2024-02-27 17:34:18 +0900

    добавлено указание дат в фильтрации через календарь

--- engine/filters.py
+++ engine/filters.py
@@ -2,8 +2,13 @@ import django_filters
 from .models import Reestr_oferts
 
 
-class ProductFilter(django_filters.FilterSet):
-    date_buhgt = django_filters.DateFromToRangeFilter()
+class ProductFilter(django_filters.FilterSet, django_filters.RangeFilter):
+    date_buhgt = django_filters.DateFromToRangeFilter(
+        widget=django_filters.widgets.RangeWidget(
+                attrs={'type': 'date'}))
+    # date_buhgt = django_filters.DateFromToRangeFilter()
+    # th_month = django_filters.ChoiceFilter()
+    # date_buhgt = django_filters.DateTimeFilter(lookup_expr='gte', label='Date')
     class Meta:
         model = Reestr_oferts
-        fields = ['exicutor', 'filial', 'date_buhgt']
+        fields = ['exicutor', 'filial']
--- engine/forms.py
+++ engine/forms.py
@@ -1,7 +1,17 @@
 from django.forms import ModelForm
 from .models import Reestr_oferts
+from django import forms
 
 class CreateOrderForm(ModelForm):
     class Meta:
         model = Reestr_oferts
         fields = ('number_orders_vozm', 'fio', 'exicutor', 'filial', 'price', 'date_buhgt', 'comment')
+
+        widgets = {
+            'date_buhgt': forms.DateInput(
+                format=('%d/%m/%Y'),
+                attrs={'class': 'form-control', 
+                       'placeholder': 'Select a date',
+                       'type': 'date'  # <--- IF I REMOVE THIS LINE, THE INITIAL VALUE IS DISPLAYED
+                      }),
+        }
