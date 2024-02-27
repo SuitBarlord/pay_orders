8869ef52838f36d467cc056b5f2579d2364d8736
SuitBarlord <dolgovNA.99@yandex.ru>
2024-02-27 17:52:30 +0900

    добавлена фильтрация даты акта

--- engine/filters.py
+++ engine/filters.py
@@ -3,9 +3,8 @@ from .models import Reestr_oferts
 
 
 class ProductFilter(django_filters.FilterSet, django_filters.RangeFilter):
-    date_buhgt = django_filters.DateFromToRangeFilter(
-        widget=django_filters.widgets.RangeWidget(
-                attrs={'type': 'date'}))
+    date_buhgt = django_filters.DateFromToRangeFilter(label='Диапозон дат передачи в бухгалтерию', widget=django_filters.widgets.RangeWidget(attrs={'type': 'date'}))
+    date_orders = django_filters.DateFromToRangeFilter(label='Диапозон дат договора', widget=django_filters.widgets.RangeWidget(attrs={'type': 'date'}))
     # date_buhgt = django_filters.DateFromToRangeFilter()
     # th_month = django_filters.ChoiceFilter()
     # date_buhgt = django_filters.DateTimeFilter(lookup_expr='gte', label='Date')
